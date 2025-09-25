97878e245c87e8845d44ba8519ddbc700ee6d80c
Davidvergara <fernanvergara@gnosiscolombia.com>
2025-09-25 17:19:27 -0500

    - El microservicio SOLICITUDES debe enviar un evento a SQS cada vez que se aprueba una solicitud.

--- domain/model/src/main/java/co/com/sti/model/sqsservices/gateways/SQSGateway.java
+++ domain/model/src/main/java/co/com/sti/model/sqsservices/gateways/SQSGateway.java
@@ -1,11 +1,12 @@
 package co.com.sti.model.sqsservices.gateways;
 
 import co.com.sti.model.sqsservices.ApplyLoanMessage;
+import co.com.sti.model.sqsservices.DataForReports;
 import co.com.sti.model.sqsservices.Notification;
 import reactor.core.publisher.Mono;
 
 public interface SQSGateway {
     Mono<Void> sendNotification(Notification notification);
     Mono<Void> sendToValidationQueue(ApplyLoanMessage message);
-    Mono<Void> sendToAprovedCount(Long idApply);
+    Mono<Void> sendToAprovedCount(DataForReports dataForReports);
 }
